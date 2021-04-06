66d9a1b4b6ad28cec1fd3b654ecf4dc0666d5802
graham-arnold-uw <graham.arnold.uw@gmail.com>
2021-04-05 21:29:22 -0700

    periodic commit of new work, not much added to learning repo. Mostly focused on django apps repo right now

---
+++ cv/align_reg_tutorial.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+#Tutorial Link: https://www.pyimagesearch.com/2020/08/31/image-alignment-and-registration-with-opencv/
+
+#This tutorial goes over how to perform image alignemnt and registration which is aligning pictures of
+#the same thing taken from difference perspectives
+
+ 
